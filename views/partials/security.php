a04ecb94d88164c50dd59c92823d1cb12c6560e0
Hamxah4U <hamxah4u@gmail.com>
2026-07-12 16:55:24 +0100

    print duplicate

--- views/partials/security.php
+++ views/partials/security.php
@@ -23,28 +23,3 @@
     }
 
     $_SESSION['last_activity'] = time();
-
-
-
-
-   /*  if (session_status() === PHP_SESSION_NONE) {
-        session_start();
-    }
-
-    $inactive_limit = 120 * 60; 
-
-    if (isset($_SESSION['last_activity'])) {
-       
-        $session_life = time() - $_SESSION['last_activity'];
-
-        if ($session_life > $inactive_limit) {
-            session_unset();
-            session_destroy();
-            
-            header("Location: /"); 
-            exit();
-        }
-    }
-
-    // Update last activity
-    $_SESSION['last_activity'] = time(); */
